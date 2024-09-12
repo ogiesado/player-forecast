d9833d5e8a07aba7910e7ed0f64d491510b8a611
Ogie Sado <sado.ogie@gmail.com>
2024-09-12 23:03:45 +0100

    Proxy raw game data

---
+++ README.md
@@ -0,0 +1,21 @@
+# Player Forecast
+
+API for providing services to [Game Statistics](https://player-statistics-xi.vercel.app/) running at [Player Forecast](#)
+
+## API
+
+`/raw-game-data`
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v1.1.27. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.ts
@@ -0,0 +1,20 @@
+Bun.serve({
+  port: 3000,
+  async fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === '/') return new Response('Player Forecasts');
+    if (url.pathname === '/raw-game-data') {
+      const gamesDataResponse = await fetch(
+        'https://fantasy.premierleague.com/api/bootstrap-static'
+      );
+
+      return Response.json(await gamesDataResponse.json(), {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+    }
+
+    return new Response('404!');
+  },
+});
